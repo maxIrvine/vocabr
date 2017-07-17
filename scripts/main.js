cef7ebf3039fb15a7785192d84dfc5e50f710e75
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-17 10:47:41 -0400

    adds synonyms array and adds data to array

--- scripts/main.js
+++ scripts/main.js
@@ -2,27 +2,29 @@ var data = [];
 var synonyms = [];
 
 function callback(d) {
-    data = d
-    console.log(data);
+    data = d;
 }
 
 function getSynonym(word){
     var url = "https://api.datamuse.com/words?ml=" + word;
     var xmlHttp = new XMLHttpRequest();
+    var count = 0;
     xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.status == 200) {
-            callback(xmlHttp.responseText);
+        if (xmlHttp.status == 200 && count < 1) {
+            callback(data = JSON.parse(xmlHttp.responseText));
+            printSynonym();
+            count++;
         }
     }
     xmlHttp.open("GET", url, true);
     xmlHttp.send(null);
-    // console.log(data);
+    
 }
 
 function printSynonym() {
     for (word in data){
         var spot = data[word];
-        console.log(spot);
+        // console.log(spot);
         synonyms.push(spot["word"]);
     }
     console.log(synonyms);
