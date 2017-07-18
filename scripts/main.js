95e97b60c5db3402c79974c5fcee64a1c8a319b1
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-18 08:53:28 -0400

    updates print statements for clarity in the printSynAnt function

--- scripts/main.js
+++ scripts/main.js
@@ -13,11 +13,11 @@ function getSynAnt(identifier, word){
     xmlHttp.onreadystatechange = function() {
         if (xmlHttp.status == 200 && count < 1) {
             callback(data = JSON.parse(xmlHttp.responseText));
-            if (identifier === 'synonym') {
-                printSynAnt("synonym");
+            if (identifier === 'syn') {
+                printSynAnt("syn");
                 count++;
             } else {
-                printSynAnt("antonym");
+                printSynAnt("ant");
             }
         }
     }
@@ -26,14 +26,19 @@ function getSynAnt(identifier, word){
 }
 
 function printSynAnt(identifier) {
+    console.log("The type of data is: " + typeof data);
     for (word in data){
         var spot = data[word];
         // console.log(spot);
-        if (identifier === "synonym") {
+        if (identifier === "syn") {
             synonyms.push(spot["word"]);
         } else {
             antonyms.push(spot["word"]);
         }
     }
-    console.log(synonyms);
+    if (identifier === "syn") {
+        console.log("The synonyms are: " + synonyms);
+    } else {
+        console.log("The antonyms are: " + antonyms);
+    }
 }
