2829db35394ad6436b5ac3c7a0a33a8151b0d954
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-21 14:17:34 -0400

    adds selectors to main.js for div tags

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 var $searchButton = $('[data-button="search"]');
 var $searchField = $('[data-input="search-bar"]');
 var $form = $('[data-form="info"]');
+var $divDefinition = $('[data-field="definition"]');
+var $divSynonym = $('[data-field="synonym"]');
+var $divAntonym = $('[data-field="antonym"]');
 
 var nearAntonyms = [];
 var antonyms = [];
