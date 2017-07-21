12c66ec2d44142d7021e0f5ec7d9a05d6d6a2741
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-21 08:47:18 -0400

    adds search function to input.js

--- scripts/input.js
+++ scripts/input.js
@@ -1,3 +1,12 @@
 var $searchButton = $('[data-button="search"]');
 var $searchField = $('[data-input="search-bar"]');
 var $form = $('[data-form="info"]');
+
+function search() {
+    $form.on("submit", function(event) {
+        event.preventDefault();
+        console.log($searchField.val());
+    });
+}
+
+search();
