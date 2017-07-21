6d66cc314bfd1358d51251cebba38699f44158a1
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-07-21 08:54:11 -0400

    adds commenting for clarity

--- scripts/input.js
+++ scripts/input.js
@@ -2,6 +2,8 @@ var $searchButton = $('[data-button="search"]');
 var $searchField = $('[data-input="search-bar"]');
 var $form = $('[data-form="info"]');
 
+//function returns whatever is entered in search bar
+//will need to integrate to main.js or use global variables
 function search() {
     $form.on("submit", function(event) {
         event.preventDefault();
